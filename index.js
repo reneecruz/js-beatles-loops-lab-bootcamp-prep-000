b60df16fbd6c3553e09e271cf5ded892d0d51c66
Renée Cruz <reneebasscruz@gmail.com>
2019-02-24 04:23:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+  emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return emptyArray;
+}
+
+
+function johnLennonFacts(facts) {
+  var newArray = [];
+  var i = 0;
+  while (i < facts.length) {
+  newArray.push(`${facts[i]} !!!`);
+  i++;
+}
+  return newArray;
+}
+
+function iLoveTheBeatles(num) {
+  
+}
